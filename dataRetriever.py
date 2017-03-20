5db0d8da3b7d990b7cb86971dffa56f1cb34cbf6
Emilio Zhao <1005874540@qq.com>
2017-03-20 10:53:57 +0800

    create data retriever initially

---
+++ dataRetriever.py
@@ -0,0 +1,48 @@
+from WindPy import *
+
+# 启动Wind API
+w.start()
+
+# 获取所有的A股 IPO > 3个月的股票
+
+# 获取周数据（有些字段参数不同，需分成若干次拉取）
+fields1 = ['close', 'mkt_cap_float', 'roa', 'mfd_buyamt_d', 'mfd_sellamt_d', 'pe', 'pb']
+# 定义某些指标需要的参数，收盘价向后复权
+option = ['traderType=1','ruleType=1','Period=W']
+
+# 获取所有A股所需数据
+print('Pull market quotation data: Start')
+security = '000566.SZ'
+startTime = '2016-12-30'
+endTime = '2017-03-20'
+
+wsd_data = w.wsd(security, fields, startTime, endTime, option)
+
+for x in wsd_data.Fields:
+        print('\t' + str(x))
+
+print('ErrorCode: ' + str(wsd_data.ErrorCode))
+
+# 生成存入Cassandra的代码，异步执行数据存储，排名计算及定期更新
+
+
+'''
+filename = 'wsd_' + security + '_' + year + '.csv'
+with open(filename, 'w', encoding='utf-8', newline='') as f:
+    f.write('DateTime')
+    for x in wsd_data.Fields:
+        f.write('\t' + str(x))
+    f.write('\n')
+    columns = len(wsd_data.Data)
+    index = len(wsd_data.Data[0])
+    for x in range(index):
+        time = str(wsd_data.Times[x])
+        f.write(time[:time.find(' ')])
+        for y in range(columns):
+            f.write('\t')
+            if wsd_data.Data[y][x] is not None:
+                f.write(str(wsd_data.Data[y][x]))
+        f.write('\n')
+
+'''
+print('Pull market quotation data: End')
