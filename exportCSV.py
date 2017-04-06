3b904c0b7206682b6fb17ea0deed3673f058fb71
Emilio <emiliocosy@163.com>
2017-04-06 14:49:45 +0800

    add Yield_rank & binary class

--- exportCSV.py
+++ exportCSV.py
@@ -1,14 +1,14 @@
 # pylint: disable=I0011,C0111, C0103,C0326,C0301, C0304, W0401,W0614
 from cassandra.cluster import Cluster
 from cassandra.util import Date
-from datetime import datetime
 import time
+import datetime
 import csv
 
 ################################################################################################
 ## Select all valid stock with required factors identified by stock code & date, saved in CSV ##
 ################################################################################################
-def export(fileName, beginDate, endDate=datetime.today().date(), factors = [], table = "factors_month"):
+def export(fileName, beginDate, endDate=datetime.datetime.today().date(), factors = [], table = "factors_month"):
     if len(factors) == 0 or beginDate > endDate:
         return
     # cassandra connection
@@ -33,13 +33,17 @@ def export(fileName, beginDate, endDate=datetime.today().date(), factors = [], t
     dateList = []
     for row in rows:
         dateList.append(row.time)
-    
+
     #total stock number
     rows = session.execute("select count(*) from "+table+" where factor='close' and time = %s ALLOW FILTERING;", [str(dateList[0])])
     for row in rows:
-        stockNum = row[0]
-        break;
-    print ("Stock number: ",stockNum)
+        validStockNum = row[0]
+        break
+    rows = session.execute('''select count(*) from stock_info''')
+    for row in rows:
+        totalStockNum = row[0]
+        break
+    print ("Total Stock: ", totalStockNum, " Valid Stock: ", validStockNum)
     # prepare SQL
     for factor in factors:
         SQL = SQL + "'"+ factor + "',"
@@ -51,23 +55,40 @@ def export(fileName, beginDate, endDate=datetime.today().date(), factors = [], t
     # open CSV file & write first line: title 
     # NOTICE:  [wb] mode won't result in problem of blank line
     with open(fileName, 'w') as csvFile:
-        names = ['id'] + factors
+        names = ['id'] + ['Yield_Rank_Class'] + factors  # column names
         f = csv.writer(csvFile, delimiter=',',lineterminator='\n')
         f.writerow(names)
-        
+
         # retrieve data
         for day in dateList:
             for stock in stocks:
                 line = []
                 rows = session.execute(preparedStmt, (stock,day))
-                
+
                 # pass when no data
                 empty = True
                 line.append(stock+'_' + str(day))
                 for row in rows:
                     empty = False
                     if row.factor.find('rank') != -1:
-                        line.append(int(row.value / stockNum * 1000)) #normalize rank value
+                        rank = int(row.value / validStockNum * 1000) # normalize rank value
+
+                        if row.factor.find('Yield') != -1:
+                            rank = int(row.value / totalStockNum * 1000)
+                            line.append(rank)
+                            ##################################################
+                            ####### CODE Area for Yield Rank Classification ##
+                            ##################################################
+                            # class 1: [1, 26]
+                            if rank > 1 * 10 and rank < 26 * 10:
+                                line.append(1)
+                            # class 0: [74, 99]
+                            elif rank > 74 * 10 and rank < 99 * 10:
+                                line.append(0)
+                            else:
+                                line.append('') #no class, fill in empty char to keep CSV well-formed
+                        else:
+                            line.append(rank)
                     else:
                         line.append(str(row.value))
                 if empty:
@@ -81,4 +102,4 @@ def export(fileName, beginDate, endDate=datetime.today().date(), factors = [], t
 
 ##############################################
 ################# USAGE EXAMPLE ##############
-export('E:\\train.csv', '2017-01-01',factors=['mkt_freeshares_rank', 'mmt_rank', 'roa_growth_rank'])
+export('E:\\train.csv', datetime.date(2017,1,1),factors=['mkt_freeshares_rank', 'mmt_rank', 'roa_growth_rank','Yield_rank'])
